28108685ab7395c1ec7683fe1472733aa91b4aec
Prathik Umbarji <68607359+PrathikUmbarji@users.noreply.github.com>
2023-10-02 10:15:53 +0530

    Update 2_to_1mux.sv

--- testbench/2_to_1mux.sv
+++ testbench/2_to_1mux.sv
@@ -1,1 +1,21 @@
 
+module tb;
+  reg a,b,sel;
+  wire c;
+  integer i;
+
+  mux_2_1 dut(.a(a),.b(b),.sel(sel),.c(c));
+
+  initial begin
+    a<=0;
+    b<=0;
+    sel<=0;
+
+    $monitor("a=%0b b=%0b sel=%0b c=%0b",a,b,sel,c);
+
+    for(i =0;i<3;i++) begin
+      {a,b,sel} = i;
+    #10;
+    end
+  end
+endmodule
